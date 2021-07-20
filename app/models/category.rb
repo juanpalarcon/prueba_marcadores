dfaa34b66de0b3ce2d08cf956310bf530edf0cd1
Juan Pablo <juanpablogast@gmail.com>
2021-07-20 19:17:41 -0400

    metodos all_children y all_parents

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,14 @@ class Category < ApplicationRecord
     belongs_to :parent, class_name: 'Category', foreign_key: :category_id, optional: true 
 
     enum status: [:privado, :publico] 
+
+    def all_children
+        Self.children.flat_map do |Child|
+      Child.all_children << Chile
+      
+      end
+      
+      Def self.all_parents
+       Where(category_id: nil)
+      End
 end
